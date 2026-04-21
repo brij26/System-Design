c6fdb6e2ae6fd93835794beed05c487db67c30b7
brij26 <brijrpatel007@gmail.com>
2026-04-21 10:40:44 +0530

    implement Singleton with locking and unlocking

---
+++ singleton_design_pattern/SingletonWithSimpleLockingAndUnlockng.cpp
@@ -0,0 +1,47 @@
+// This is ok when our class is very expensive to store in memory
+// but if our class size is not that much and it is not that much expensive to store in the memory than there is no use of locking and unlocking we can make our code thread safe by just eager initialization of our class
+// for example so the next code
+
+#include <iostream>
+#include <mutex>
+
+using namespace std;
+
+class Singleton
+{
+private:
+    static Singleton *instance;
+    static mutex mtx;
+
+    Singleton()
+    {
+        cout << "Singleton Constructor Called!" << endl;
+    }
+
+public:
+    // Double check locking..
+    static Singleton *getInstance()
+    {
+        if (instance == nullptr)
+        {                                // First check (no locking)
+            lock_guard<mutex> lock(mtx); // Lock only if needed
+            if (instance == nullptr)
+            { // Second check (after acquiring lock)
+                instance = new Singleton();
+            }
+        }
+        return instance;
+    }
+};
+
+// Initialize static members
+Singleton *Singleton::instance = nullptr;
+mutex Singleton::mtx;
+
+int main()
+{
+    Singleton *s1 = Singleton::getInstance();
+    Singleton *s2 = Singleton::getInstance();
+
+    cout << (s1 == s2) << endl;
+}
