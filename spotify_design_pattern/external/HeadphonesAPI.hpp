a74bce61405a0cc5ab1821ba5b67e2db905d4964
brij26 <brijrpatel007@gmail.com>
2026-06-02 12:00:02 +0530

    implement spotify system design pattern

---
+++ spotify_design_pattern/external/HeadphonesAPI.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class HeadphonesAPI
+{
+public:
+    void playSoundViaJack(const string &data)
+    {
+        cout << "[Headphones] Playing: " << data << "\n";
+        // mimics playing music
+    }
+};
